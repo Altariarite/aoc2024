e7a7fdedb7028ac50e00e567625503cdc5639e0a
altaria <yuanting.mao@gmail.com>
2024-12-01 13:10:02 +0000

    Add part2

--- d1.ex
+++ d1.ex
@@ -1,13 +1,24 @@
 defmodule D1 do
-  def d1() do
-    {:ok, contents} = File.read("d1")
-
-    contents
+  def parse_lists() do
+    File.read!("d1")
     |> String.split("\n", trim: true)
     |> Enum.map(&String.split/1)
     |> Enum.zip_with(&Function.identity/1)
+    |> Enum.map(fn l -> Enum.map(l, &String.to_integer/1) end)
+  end
+
+  def distance(list) do
+    list
     |> Enum.map(&Enum.sort/1)
-    |> Enum.zip_with(fn [a, b] -> abs(String.to_integer(b) - String.to_integer(a)) end)
+    |> Enum.zip_with(fn [a, b] -> abs(a - b) end)
+    |> Enum.sum()
+  end
+
+  def similarity([l, r]) do
+    freq = Enum.frequencies(r)
+
+    l
+    |> Enum.map(&(&1 * Map.get(freq, &1, 0)))
     |> Enum.sum()
   end
 end
